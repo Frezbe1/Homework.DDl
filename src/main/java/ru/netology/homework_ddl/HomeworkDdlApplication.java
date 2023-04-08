ffa9059c4b2a17f23422af1448ffcd22e97abecd
Vitaly Shalaginov <v.a.sh@mail.ru>
2023-04-08 14:37:22 +0700

    DDL

---
+++ src/main/java/ru/netology/homework_ddl/HomeworkDdlApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.homework_ddl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkDdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkDdlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/homework_ddl/HomeworkDdlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.homework_ddl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkDdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
